1c100a3f11a2a30b29339a3ccb87cdb2ace93120
Andy Michels <andy.michels@garmin.com>
2016-12-14 22:33:01 -0600

    jquery added

--- src/src-js/app.js
+++ src/src-js/app.js
@@ -1,6 +1,13 @@
 import Nav from "./modules/Nav";
+import $ from 'jquery';
+import jQuery from 'jquery';
 
+// Site setup
 window.site = window.site || {};
 let _this = window.site
+
+// jQuery setup
+window.$ = $;
+window.jQuery = jQuery;
 
 _this.nav = new Nav;
